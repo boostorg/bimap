6f26572aaea692fc44632274f08c9e316f7eaf93
joaquintides <joaquin@tid.es>
2026-03-29 19:50:05 +0200

    tested and documented std::initializer_list ctor

--- test/test_bimap_unordered.cpp
+++ test/test_bimap_unordered.cpp
@@ -1,7 +1,7 @@
 // Boost.Bimap
 //
 // Copyright (c) 2006-2007 Matias Capeletto
-// Copyright (c) 2024 Joaquin M Lopez Munoz
+// Copyright (c) 2024-2026 Joaquin M Lopez Munoz
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
@@ -77,6 +77,7 @@ void test_bimap()
 
         bm_type bm;
 
+        test_bimap_init_copy_swap<bm_type>(data) ;
         test_unordered_set_unordered_multiset_bimap(
             bm,data,left_data,right_data
         );
